a04cb9b11506f865e998fdbd83bccf1b30f2639d
JBris <james.a.bristow.contact@gmail.com>
2020-04-13 04:11:41 +1200

    Adding search results scaffolding

--- client/src/components/SearchForm.vue
+++ client/src/components/SearchForm.vue
@@ -4,21 +4,36 @@
         Search
         <!-- 1 -->
         <input type="text" v-model="project" class="form-control" placeholder="search...">
+        <div v-if="$apollo.queries.gitRepos.loading">Searching...</div>
         <div v-if="error">{{ error }}</div>
+        <div class="row">
+            <div class="col-md-3" v-for="repo in gitRepos" v-bind:key="`${repo.id}-${repo.author}`">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                    <!-- display the city name and country  -->
+                        {{ repo.repo }}: {{ repo.author }} 
+                    </div>
+                    <div class="panel-body">
+                    <!-- display the latitude and longitude of the city  -->
+                        <p>{{ repo.htmlUrl }},{{ repo.description }}.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
   </div>
   
 </template>
 
 <script>
-
-  // 2
   import { SEARCH_QUERY } from '@/graphql'
 
   export default {
     name: 'Search',
     data () {
       return {
+        gitRepos: [],
         provider: 'GITHUB',
         project: '',
         error: null,
@@ -31,8 +46,11 @@
           return {
             provider: this.provider,
             project: this.project,
-            quantity: 100,
+            quantity: 50,
           }
+        },
+        update (data) {
+            return data.gitRepos.items;
         },
         options: () => ({ errorPolicy: 'all' }),
         skip () {
@@ -41,7 +59,7 @@
         error(error) {
             this.error = error.message;
         },
-        debounce: 500, 
+        debounce: 300, 
         errorPolicy: 'all',
       }
     }
